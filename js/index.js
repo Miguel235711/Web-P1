0e23141bf1a0c482f2157b49fc6a70fbc9184d65
Miguel235711 <a01423629@itesm.mx>
2021-03-20 09:36:55 -0600

    tiny change

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-data = [[1,2,3,4,5],[6,7,8,9,10]]
+data = [/*[1,2,3,4,5],[6,7,8,9,10]*/]
 
 const updateDataInHTML = ()=> {
     document.querySelector('data-table').setAttribute('data',JSON.stringify(data))
